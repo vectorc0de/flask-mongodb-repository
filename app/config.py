91954174b0f676e220b86f9d135fd3b622b232a7
Victor Gomez <vgomez@containerdevs.com>
2025-07-29 11:38:26 -0400

    fix config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MONGO_SERV = os.environ.get('MONGO_SERV') or 'localh0t'
+    MONGO_USR = os.environ.get('MONGO_USR') or 'vectorc0de'
+    MONGO_PWD = os.environ.get('MONGO_PWD') or 'miakhalifa'
+
     MONGODB_SETTINGS = {
         'db': 'job_board',
         'host': os.environ.get('MONGODB_URI') or 'mongodb://localhost/job_board'
